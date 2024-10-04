34a8c9b5145a228641ed06ea5acf0171f896e974
innfi <innfi.world@gmail.com>
2024-10-04 21:02:07 +0900

    reviving mongo before removing its dependencies

--- basic-backend/src/coupon/module.ts
+++ basic-backend/src/coupon/module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
 
 import { CouponController } from './controller';
 import { CounponService } from './service';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.registerQueue({ name: 'request_queue' }),
+    BullModule.registerQueue({ name: 'response_queue' }),
+  ],
   controllers: [CouponController],
   providers: [CounponService],
   exports: [CounponService],
